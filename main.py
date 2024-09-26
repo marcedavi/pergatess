b767c94565882907edbc09fed5521abf39484d1d
Marcello Davi <davimarcello98@gmail.com>
2024-09-26 03:50:56 +0200

    path fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from pathlib import Path
 
 os.environ['PATH'] += os.pathsep + os.path.join(Path(__file__).parent.resolve(), 'dependencies', 'poppler', 'Library', 'bin')
-os.environ['TESSDATA_PREFIX'] = os.path.join(Path(__file__).parent.resolve(), 'tesseract-5.3.4', 'tessdata')
+os.environ['TESSDATA_PREFIX'] = os.path.join(Path(__file__).parent.resolve(), 'tesseract-5.3.4', 'tessdata').replace("\\", "/")
 
 def choose_folder(entry):
     folder_path = filedialog.askdirectory(initialdir=Path.home())
